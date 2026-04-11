8013e97f213e389852e3bf3ab25734bddeb9af03
Prince Edward  G. <guevaraprinceedward@gmail.com>
2026-04-11 21:27:52 +0800

    Update checkout.php

---
+++ checkout.php
@@ -0,0 +1,32 @@
+<?php include 'config.php'; ?>
+
+<?php
+if(!isset($_SESSION['user_id'])){
+    header("Location: login.php");
+    exit;
+}
+
+$total = 0;
+
+foreach($_SESSION['cart'] as $id => $qty){
+    $res = $conn->query("SELECT * FROM products WHERE id=$id");
+    $row = $res->fetch_assoc();
+    $total += $row['price'] * $qty;
+}
+
+// INSERT ORDER
+$conn->query("INSERT INTO orders (user_id,total)
+VALUES ({$_SESSION['user_id']}, $total)");
+
+$order_id = $conn->insert_id;
+
+// INSERT ITEMS
+foreach($_SESSION['cart'] as $id => $qty){
+    $conn->query("INSERT INTO order_items (order_id,product_id,quantity)
+    VALUES ($order_id,$id,$qty)");
+}
+
+unset($_SESSION['cart']);
+
+echo "<h2>✅ Order placed!</h2>";
+echo "<a href='orders.php'>View Orders</a>";
